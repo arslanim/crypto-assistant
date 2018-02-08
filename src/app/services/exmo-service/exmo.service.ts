d6a26bb9b80c1704a6857173fdfd48e0ac193658
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2018-02-09 01:50:37 +0400

    Exmo service
    updated methods - now get rate method returns not just json but prepared rate model

--- src/app/services/exmo-service/exmo.service.ts
+++ src/app/services/exmo-service/exmo.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import {HttpClient, HttpErrorResponse, HttpEvent} from "@angular/common/http";
 import { EXMO_SERVICE_CONFIG } from "./exmo.service.config";
 import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+import {Rate} from "../../models/rate.model";
 
 @Injectable()
 export class ExmoService {
@@ -12,8 +15,35 @@ export class ExmoService {
     this.apiUrl = EXMO_SERVICE_CONFIG.apiUrl;
   }
 
-  getTicker(): Observable<any> {
+  getRatePairs(): Observable<string[]> {
+    return this.http.get('./assets/test-data/ticker.json').map(
+      (response: any) => {
+        let ratePairs = [];
+
+        for (let rateName in response) {
+          ratePairs.push(rateName);
+        }
+
+        return ratePairs;
+      }
+    );
+  }
+
+  getRateByName(rateName: string): Observable<Rate> {
     //return this.http.get(this.apiUrl.baseUrl + this.apiUrl.ticker)
-    return this.http.get('./assets/test-data/ticker.json');
+    return this.http.get('./assets/test-data/ticker.json').map(
+      (response) => {
+        let rate: Rate;
+
+        for (let name in response) {
+          if (name === rateName) {
+            rate = new Rate().deserialize(response[name]);
+            rate.name = name;
+          }
+        }
+
+        return rate;
+      }
+    );
   }
 }
